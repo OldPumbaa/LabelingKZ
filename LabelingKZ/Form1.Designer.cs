e8c5761a0d7b1e9c2eaea9856f4696dadc4dbbfe
Potapov Dmitriy <dr.red0742@ya.ru>
2025-01-03 16:30:57 +0500

    ...

--- LabelingKZ/Form1.Designer.cs
+++ LabelingKZ/Form1.Designer.cs
@@ -56,11 +56,11 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(280, 101);
+            this.label2.Location = new System.Drawing.Point(273, 101);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(31, 16);
+            this.label2.Size = new System.Drawing.Size(43, 16);
             this.label2.TabIndex = 2;
-            this.label2.Text = "v1.2";
+            this.label2.Text = "v1.2fix";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // Form1
